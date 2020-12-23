4acc42b14f2d59ac152cef755c3d13a055ed0736
devcustrom <devcustrom@gmail.com>
2020-12-23 21:10:14 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -125,6 +125,20 @@ function menu_unchk(){
     else {menu_unchk();}
   });
 grecaptcha.execute();
+const anchors = document.querySelectorAll('a[href*="#"]')
+
+for (let anchor of anchors) {
+  anchor.addEventListener('click', function (e) {
+    e.preventDefault()
+    
+    const blockID = anchor.getAttribute('href').substr(1)
+    
+    document.getElementById(blockID).scrollIntoView({
+      behavior: 'smooth',
+      block: 'start'
+    })
+  })
+}
 };
 function enablebtn(token) {
   for (var i = 0; i < getbtncls.length; i++) {
